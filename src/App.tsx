d949e7c53b0ed262b8e5f1df0aa4930c3b0ed880
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-09-11 14:24:38 +0800

    initial App setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,34 @@
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+import { Note } from './types/types';
+import notesService from './services/notes';
+import NoteComponent from './components/NoteComponent';
 
 function App() {
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  const getAllNotes = async () => {
+    try {
+      const notes = await notesService.getAllNotes();
+      setNotes(notes);
+      console.log(notes)
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getAllNotes();
+  }, []);
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      {notes.map((note) => (
+        <NoteComponent note={note} />
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
